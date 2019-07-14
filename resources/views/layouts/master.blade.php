49e47f14f396bcb7daf4db98057dc1ddefab8865
Ahmed Helmy <helmyahmed248@gmail.com>
2019-07-14 14:51:58 +0200

    First commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>AdminLTE 3 | Starter</title>
+
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+
+{{-- to include navbar --}}
+  @include('layouts.nav')
+
+{{--  include sidebar --}}
+  @include('layouts.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+          @yield('header')
+
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+
+        <div class="row">
+                @yield('content')
+
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  
+  {{-- footer --}}
+  @include('layouts.footer')
+
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<script src="/js/app.js"></script>
+</body>
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+var path = require('path');
+var webpack = require('webpack');
+var Mix = require('laravel-mix').config;
+var plugins = require('laravel-mix').plugins;
+
+
+
+Mix.initialize();
